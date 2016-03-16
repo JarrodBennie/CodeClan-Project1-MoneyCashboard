dbfbf96152d384271fdb01167e397e002e2e6901
Jarrod Bennie <jarrodbennie@icloud.com>
2016-03-16 18:17:10 +0000

    improves transaction testing

--- specs/transaction_spec.rb
+++ specs/transaction_spec.rb
@@ -4,19 +4,23 @@ require_relative "../models/transaction"
 
 class TestTransaction < MiniTest::Test
   def setup
-    params = { "amount" => 100, "transaction_date" => "2016-03-11" }
-    @transaction1 = Transaction.new( params )
+    params = { "amount" => 99.90, "transaction_date" => "2016-03-11" }
+    @transaction = Transaction.new( params )
   end
 
   def test_amount
-    expectation = 100
-    result = @transaction1.amount
-    assert_equal( expectation, result )
+    assert_equal( 99.90, @transaction.amount )
   end
 
   def test_date
-    expectation = "2016-03-11"
-    result = @transaction1.transaction_date
-    assert_equal( expectation, result )
+    assert_equal( "2016-03-11", @transaction.transaction_date )
+  end
+
+  def test_date_format
+    assert_equal( "11/03/2016", @transaction.date_format )
+  end
+
+  def test_amount_format
+    assert_equal( "99.90", @transaction.amount_format )
   end
 end
