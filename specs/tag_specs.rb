5e1f1577c3867605d39a594c7b2efd98069194e2
Jarrod Bennie <jarrodbennie@icloud.com>
2016-03-16 18:19:08 +0000

    improves tag testing

--- specs/tag_specs.rb
+++ specs/tag_specs.rb
@@ -5,12 +5,10 @@ require_relative "../models/tag"
 class TestTag < MiniTest::Test
   def setup
     params = { "name" => "Groceries" }
-    @tag1 = Tag.new( params )
+    @tag = Tag.new( params )
   end
 
   def test_name
-    expectation = "Groceries"
-    result = @tag1.name
-    assert_equal( expectation, result ) 
+    assert_equal( "Groceries", @tag.name ) 
   end
 end
