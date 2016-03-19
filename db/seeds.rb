6058ca7ef3275bc16e22f4baeb4376e5a639e2dd
Jarrod Bennie <jarrodbennie@icloud.com>
2016-03-19 02:15:04 +0000

    updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
-require_relative "../models/merchant"
-require_relative "../models/tag"
-require_relative "../models/transaction"
+require_relative "../models/account"
 
 Merchant.delete_all
 Tag.delete_all
