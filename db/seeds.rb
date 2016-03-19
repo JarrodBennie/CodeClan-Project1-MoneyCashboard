2c54cd91ef3f57e21fb8598db9e4bd061a4565de
Jarrod Bennie <jarrodbennie@icloud.com>
2016-03-19 02:26:27 +0000

    updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,24 @@
 require_relative "../models/account"
 
+Transaction.delete_all
 Merchant.delete_all
 Tag.delete_all
-Transaction.delete_all
 
 merchant1 = Merchant.create( "name" => "Tesco" )
 merchant2 = Merchant.create( "name" => "Scottish Power" )
 merchant3 = Merchant.create( "name" => "National Rail" )
 merchant4 = Merchant.create( "name"=> "TravelLodge" )
 merchant5 = Merchant.create( "name" => "Boots" )
+merchant6 = Merchant.create( "name" => "CodeClan" )
+merchant7 = Merchant.create( "name" => "Waitrose" )
 
 tag1 = Tag.create( "name" => "Groceries" )
 tag2 = Tag.create( "name" => "Utilities" )
 tag3 = Tag.create( "name" => "Travel" )
 tag4 = Tag.create( "name" => "Hospitality" )
 tag5 = Tag.create( "name" => "Medical" )
+tag6 = Tag.create( "name" => "Education" )
+tag7 = Tag.create( "name" => "Business" )
 
 transaction1 = Transaction.create(
   "amount" => 103.27,
@@ -62,5 +66,19 @@ transaction7 = Transaction.create(
   "amount" => 18.51,
   "transaction_date" => "7-March-16",
   "merchant_id" => merchant1.id,
+  "tag_id" => tag1.id
+)
+
+transaction8 = Transaction.create(
+ "amount" => 1000,
+ "transaction_date" => "11-March-16",
+ "merchant_id" => merchant6.id,
+ "tag_id" => tag6.id
+)
+
+transaction9 = Transaction.create(
+  "amount" => 26.91,
+  "transaction_date" => "28-February-16",
+  "merchant_id" => merchant7.id,
   "tag_id" => tag1.id
 )
