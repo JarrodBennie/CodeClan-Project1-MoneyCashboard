822076af69c617d7239b12cc27eca75e418a54a4
Jarrod Bennie <jarrodbennie@icloud.com>
2016-03-20 02:31:51 +0000

    adds monthly stats to homepage

--- app.rb
+++ app.rb
@@ -9,11 +9,14 @@ require_relative "controllers/tags"
 
 get "/" do
   options = { "transactions" => Transaction.all, "merchants" => Merchant.all, "tags" => Tag.all }
-  @account = Account.new( options )
-  if @account.merchants.size == 0 || @account.tags.size == 0
+  @account_life = Account.new( options )
+
+  options = { "transactions" => Transaction.find_this_month, "merchants" => Merchant.all, "tags" => Tag.all }
+  @account_month = Account.new( options )
+  if @account_life.merchants.size == 0 || @account_life.tags.size == 0
     erb :home_new_user
   elsif 
-    @account.transactions.size == 0
+    @account_life.transactions.size == 0
     erb :home_no_transactions
   else
     erb :home
