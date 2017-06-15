5efb457116107fc18aa82cd551f3f20f6fc6931c
JarrodBennie <jarrodbennie@icloud.com>
2017-06-15 11:38:47 +0100

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,105 +21,105 @@ tag5 = Tag.create( "name" => "Education", "monthly_budget" => 1000.00 )
 
 transaction1 = Transaction.create(
   "amount" => 103.27,
-  "transaction_date" => "14-March-16",
+  "transaction_date" => "14-June-17",
   "merchant_id" => merchant1.id,
   "tag_id" => tag1.id
 )
 
 transaction2 = Transaction.create(
   "amount" => 75.50,
-  "transaction_date" => "13-March-16",
+  "transaction_date" => "13-June-17",
   "merchant_id" => merchant2.id,
   "tag_id" => tag2.id
 )
 
 transaction3 = Transaction.create(
   "amount" => 450.00,
-  "transaction_date" => "10-March-16",
+  "transaction_date" => "10-June-17",
   "merchant_id" => merchant3.id,
   "tag_id" => tag2.id
 )
 
 transaction4 = Transaction.create(
   "amount" => 62.50,
-  "transaction_date" => "7-March-16",
+  "transaction_date" => "7-June-17",
   "merchant_id" => merchant4.id,
   "tag_id" => tag2.id
 )
 
 transaction5 = Transaction.create(
   "amount" => 1500,
-  "transaction_date" => "14-February-16",
+  "transaction_date" => "14-May-17",
   "merchant_id" => merchant6.id,
   "tag_id" => tag5.id
 )
 
 transaction6 = Transaction.create(
   "amount" => 24.99,
-  "transaction_date" => "11-March-16",
+  "transaction_date" => "11-June-17",
   "merchant_id" => merchant1.id,
   "tag_id" => tag1.id
 )
 
 transaction7 = Transaction.create(
   "amount" => 18.51,
-  "transaction_date" => "7-March-16",
+  "transaction_date" => "7-June-17",
   "merchant_id" => merchant1.id,
   "tag_id" => tag1.id
 )
 
 transaction8 = Transaction.create(
  "amount" => 1000,
- "transaction_date" => "11-March-16",
+ "transaction_date" => "11-June-17",
  "merchant_id" => merchant6.id,
  "tag_id" => tag5.id
 )
 
 transaction9 = Transaction.create(
   "amount" => 26.91,
-  "transaction_date" => "28-February-16",
+  "transaction_date" => "28-May-17",
   "merchant_id" => merchant7.id,
   "tag_id" => tag1.id
 )
 
 transaction10 = Transaction.create(
   "amount" => 41.60,
-  "transaction_date" => "22-February-16",
+  "transaction_date" => "22-May-17",
   "merchant_id" => merchant1.id,
   "tag_id" => tag1.id
 )
 
 transaction11 = Transaction.create(
   "amount" => 4.28,
-  "transaction_date" => "23-February-16",
+  "transaction_date" => "23-May-17",
   "merchant_id" => merchant8.id,
   "tag_id" => tag1.id
 )
 
 transaction12 = Transaction.create(
   "amount" => 4.28,
-  "transaction_date" => "17-March-16",
+  "transaction_date" => "17-June-17",
   "merchant_id" => merchant8.id,
   "tag_id" => tag1.id
 )
 
 transaction13 = Transaction.create(
   "amount" => 22.50,
-  "transaction_date" => "19-March-16",
+  "transaction_date" => "19-June-17",
   "merchant_id" => merchant5.id,
   "tag_id" => tag4.id
 )
 
 transaction14 = Transaction.create(
   "amount" => 22.50,
-  "transaction_date" => "4-March-16",
+  "transaction_date" => "4-June-17",
   "merchant_id" => merchant5.id,
   "tag_id" => tag4.id
 )
 
 transaction15 = Transaction.create(
   "amount" => 22.50,
-  "transaction_date" => "12-February-16",
+  "transaction_date" => "12-May-17",
   "merchant_id" => merchant5.id,
   "tag_id" => tag4.id
 )
--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -3,7 +3,7 @@ require "pg"
 class  SqlRunner
   def self.execute( query )
     begin
-      db = PG.connect( dbname: "MoneyCashboard", host: "localhost" )
+      db = PG.connect( dbname: "money_cashboard", host: "localhost" )
       result = db.exec( query )
     ensure
       db.close
